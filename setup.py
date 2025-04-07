49378f6ce2f57d5dca0bd5969af0627087f1d737
ebeirne <ethan.g.beirne@gmail.com>
2025-04-07 09:07:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # Adjust license if needed
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
